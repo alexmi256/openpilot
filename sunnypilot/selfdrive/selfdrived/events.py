82cf68e5ab9bed99f180cb3928896d3e9af350b8
nayan <nayan8teen@gmail.com>
2025-09-24 19:19:16 -0400

    Longitudinal: Speed Limit Assist (SLA) #833

--- sunnypilot/selfdrive/selfdrived/events.py
+++ sunnypilot/selfdrive/selfdrived/events.py
@@ -3,7 +3,7 @@ from cereal import log, car, custom
 from openpilot.common.constants import CV
 from openpilot.sunnypilot.selfdrive.selfdrived.events_base import EventsBase, Priority, ET, Alert, \
   NoEntryAlert, ImmediateDisableAlert, EngagementAlert, NormalPermanentAlert, AlertCallbackType, wrong_car_mode_alert
-from openpilot.sunnypilot.selfdrive.controls.lib.speed_limit import REQUIRED_INITIAL_MAX_SET_SPEED, CRUISE_SPEED_TOLERANCE
+from openpilot.sunnypilot.selfdrive.controls.lib.speed_limit import PCM_LONG_REQUIRED_MAX_SET_SPEED
 
 
 AlertSize = log.SelfdriveState.AlertSize
@@ -29,13 +29,18 @@ def speed_limit_adjust_alert(CP: car.CarParams, CS: car.CarState, sm: messaging.
 
 
 def speed_limit_pre_active_alert(CP: car.CarParams, CS: car.CarState, sm: messaging.SubMaster, metric: bool, soft_disable_time: int, personality) -> Alert:
-  max_initial_set_speed = round(REQUIRED_INITIAL_MAX_SET_SPEED * (CV.MS_TO_KPH if metric else CV.MS_TO_MPH))
-  if metric:
-    max_initial_set_speed += round(CRUISE_SPEED_TOLERANCE * CV.MS_TO_KPH)
-  speed_unit = "km/h" if metric else "mph"
+  if CP.openpilotLongitudinalControl and CP.pcmCruise:
+    # PCM long
+    pcm_long_required_max_set_speed_conv = round(PCM_LONG_REQUIRED_MAX_SET_SPEED[metric] * (CV.MS_TO_KPH if metric else CV.MS_TO_MPH))
+    speed_unit = "km/h" if metric else "mph"
+    alert_2_str = f"Manually change set speed to {pcm_long_required_max_set_speed_conv} {speed_unit} to activate"
+  else:
+    # Non PCM long
+    alert_2_str = "Operate the +/- cruise control button to activate"
+
   return Alert(
     "Speed Limit Assist: Activation Required",
-    f"Manually change set speed to {max_initial_set_speed} {speed_unit} to activate",
+    alert_2_str,
     AlertStatus.normal, AlertSize.mid,
     Priority.LOW, VisualAlert.none, AudibleAlert.none, .1)
 
